cbcfde37c01c45319547a2115479fbace01694e4
loganagol <lafeid.314@gmail.com>
2022-02-25 17:00:01 -0500

    Basic calculator functionality. Can input numbers, make operator, and get result.

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -1,3 +1,8 @@
+/**
+ * References:
+ *    https://www.freecodecamp.org/news/how-to-build-an-html-calculator-app-from-scratch-using-javascript-4454b8714b98/
+ */
+
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator-keys");
 const display = document.querySelector(".calculator-display");
@@ -9,12 +14,18 @@ keys.addEventListener("click", (e) => {
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    // console.log(previousKeyType);
+    console.log(calculator.dataset);
 
     if (!action) {
       console.log("NUMBER KEY");
 
-      if (displayedNum == "0") {
+      if (displayedNum === "0" || previousKeyType === "operator") {
         display.textContent = keyContent;
+        calculator.dataset.previousKeyType = "";
+
+        calculator.dataset.firstValue = displayedNum;
       } else {
         display.textContent = displayedNum + keyContent;
       }
@@ -26,6 +37,10 @@ keys.addEventListener("click", (e) => {
       action === "divide"
     ) {
       console.log("OPERATOR KEY");
+
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
     if (action === "decimal") {
       console.log("DECIMAL KEY");
@@ -37,6 +52,32 @@ keys.addEventListener("click", (e) => {
     }
     if (action === "calculate") {
       console.log("EQUALS KEY");
+
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
   }
 });
+
+const calculate = (n1, operator, n2) => {
+  console.log(operator + "ing " + n1 + " and " + n2);
+
+  let result = "";
+  let num1 = parseFloat(n1);
+  let num2 = parseFloat(n2);
+
+  if (operator === "add") {
+    result = num1 + num2;
+  } else if (operator === "subtract") {
+    result = num1 - num2;
+  } else if (operator === "multiply") {
+    result = num1 * num2;
+  } else if (operator === "divide") {
+    result = num1 / num2;
+  }
+
+  return result;
+};
