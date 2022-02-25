d626d19104aa43eef7a6ef82c056fea50f90e7d7
loganagol <lafeid.314@gmail.com>
2022-02-25 18:26:12 -0500

    Calculator can now perform repetitive calculations on current numbers by repeatedly pressing operator and equals keys.

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -83,14 +83,19 @@ keys.addEventListener("click", (e) => {
     if (action === "calculate") {
       console.log("EQUALS KEY");
 
-      const firstValue = calculator.dataset.firstValue;
+      let firstValue = calculator.dataset.firstValue;
       const operator = calculator.dataset.operator;
-      const secondValue = displayedNum;
+      let secondValue = displayedNum;
 
       if (firstValue) {
+        if (previousKeyType === "calculate") {
+          firstValue = displayedNum;
+          secondValue = calculator.dataset.modValue;
+        }
         display.textContent = calculate(firstValue, operator, secondValue);
       }
 
+      calculator.dataset.modValue = secondValue;
       calculator.dataset.previousKeyType = "calculate";
     }
   }
