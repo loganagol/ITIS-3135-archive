34a71d9ec8a2932e771726edbff6d13b56ebe252
loganagol <lafeid.314@gmail.com>
2022-03-24 00:14:58 -0400

    Added function to automatically advance slideshow at interval. Manually selecting image will pause slideshow.

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,20 +1,54 @@
-$(document).ready(function() {
-    console.log("! document ready");
+$(document).ready(function () {
+  let currentIndex = 0;
 
-    $(".slideshow-images a").each(function() {
-        let swappedImage = new Image();
-        swappedImage.src = $(this).attr("href");
-    });
+  console.log("! document ready");
 
-    $(".slideshow-images a").click(function(event) {
-        console.log("! image clicked");
+  let intervalID = setInterval(function () {
+    console.log("! slideshow change");
 
-        event.preventDefault();
+    if (currentIndex > 5) currentIndex = 0;
 
-        let imagePath = $(this).attr("href");
-        $("#image-main").attr("src", imagePath);
-        
-        let title = $(this).attr("title");
-        $("#image-title").text(title);
-    });
+    const imagePaths = [
+      "images/trip_1.jpg",
+      "images/trip_2.jpg",
+      "images/trip_3.jpg",
+      "images/trip_4.jpg",
+      "images/trip_5.jpg",
+      "images/trip_6.jpg",
+    ];
+
+    const imageDesc = [
+      "St Louis Bridge",
+      "Nebraska Plains",
+      "Utah Canyons",
+      "The Road to Delta",
+      "Sevier Lakebed",
+      "Basin Sunrise",
+    ];
+
+    $("#image-main").attr("src", imagePaths[currentIndex]);
+    $("#image-title").text(imageDesc[currentIndex]);
+    currentIndex += 1;
+
+    console.log(currentIndex);
+  }, 2000);
+
+  $(".slideshow-images a").each(function () {
+    let swappedImage = new Image();
+    swappedImage.src = $(this).attr("href");
+  });
+
+  $(".slideshow-images a").click(function (event) {
+    console.log("! image clicked");
+
+    event.preventDefault();
+
+    let imagePath = $(this).attr("href");
+    $("#image-main").attr("src", imagePath);
+
+    let title = $(this).attr("title");
+    $("#image-title").text(title);
+
+    clearInterval(intervalID); // "stop" slideshow
+  });
 });
