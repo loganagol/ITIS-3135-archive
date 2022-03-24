a4401b71de671e5f5d7a38e8816a95d3955f7550
loganagol <lafeid.314@gmail.com>
2022-03-24 13:06:29 -0400

    Added timeline that registers clicks.

--- itis3135/project/scripts/timeline.js
+++ itis3135/project/scripts/timeline.js
@@ -1,22 +1,7 @@
-const inputs = document.querySelectorAll(".input");
-const years = document.querySelectorAll(".description_container li");
+const inputs = document.querySelectorAll(".input-year");
 
 inputs.forEach((el) =>
   el.addEventListener("click", (e) => {
-    console.log("clicked");
-    console.log(el)
+    console.log("! clicked " + el.innerHTML);
   })
 );
-
-// $(function(){
-// 	var inputs = $('.input');
-// 	var paras = $('.description-flex-container').find('p');
-// 	inputs.click(function(){
-// 		var t = $(this),
-// 				ind = t.index(),
-// 				matchedPara = paras.eq(ind);
-		
-// 		t.add(matchedPara).addClass('active');
-// 		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
-// 	});
-// });
