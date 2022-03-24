76fc48471776c34d39e32e24111150497e5a9ed3
loganagol <lafeid.314@gmail.com>
2022-03-24 13:14:41 -0400

    Timeline years now highlight when clicked. Descriptions are hidden by default.

--- itis3135/project/scripts/timeline.js
+++ itis3135/project/scripts/timeline.js
@@ -3,5 +3,10 @@ const inputs = document.querySelectorAll(".input-year");
 inputs.forEach((el) =>
   el.addEventListener("click", (e) => {
     console.log("! clicked " + el.innerHTML);
+
+    inputs.forEach((el) => {
+      el.classList.remove("active");
+    });
+    el.classList.add("active");
   })
 );
