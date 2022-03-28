7000988b1807871e5a073dd81944e78d75168bcb
loganagol <lafeid.314@gmail.com>
2022-03-28 19:44:42 -0400

    Preloaded images and set up event handlers for each link. Cancelled default action.

--- itis3135/Feid-Activity10/Feid-Activity10.js
+++ itis3135/Feid-Activity10/Feid-Activity10.js
@@ -1,15 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // preload the image for each link
+  const imagePaths = [
+    "Feid-Activity10_images/h1.jpg",
+    "Feid-Activity10_images/h2.jpg",
+    "Feid-Activity10_images/h3.jpg",
+    "Feid-Activity10_images/h4.jpg",
+    "Feid-Activity10_images/h5.jpg",
+    "Feid-Activity10_images/h6.jpg",
+  ];
 
+  let $images = [];
+  for (let i = 0; i < imagePaths.length; i++) {
+    $images.push($("<img>").attr("src", imagePaths[i]));
+  }
+  console.log("! preloaded images");
 
-        // preload the image for each link
-
-        // set up the event handlers for each link
-
-    			// get the image URL and caption for each image and animate the caption
-
-            // cancel the default action of each link
-
+  // set up the event handlers for each link
+  $("ul li a").click(function (evt) {
+    console.log("! image clicked");
+    
+    // get the image URL and caption for each image and animate the caption
+    
+    
+    // cancel the default action of each link
+    evt.preventDefault();
 
     // move the focus to the first link
-
+  });
 }); // end ready
