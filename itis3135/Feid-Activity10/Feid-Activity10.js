278e295db8cb15720f52ac33743d52a4389be39d
loganagol <lafeid.314@gmail.com>
2022-03-28 22:09:53 -0400

    Made the caption animate.

--- itis3135/Feid-Activity10/Feid-Activity10.js
+++ itis3135/Feid-Activity10/Feid-Activity10.js
@@ -15,25 +15,29 @@ $(document).ready(function () {
   }
   console.log("! preloaded images");
 
+  $("#caption").animate({ fontSize: "2em" }, 3000, function () {});
+
   // set up the event handlers for each link
   $("ul li a").click(function (evt) {
     console.log("! image clicked");
+    let imagePath = $(this).attr("href");
+    let caption = $(this).attr("title");
 
     // Then add statements that fade the caption and image out over a
     // duration of three seconds.
-    let imagePath = $(this).attr("href");
-    $("#image").fadeOut(3000, function () {
-      $("#image").attr("src", imagePath);
-      $("#image").fadeIn(3000);
-    });
+    $("#caption").animate({ fontSize: "1em" }, 3000, function () {
+      $("#image").fadeOut(3000, function () {
+        $("#image").attr("src", imagePath);
+        $("#image").fadeIn(3000);
+      });
 
-    let caption = $(this).attr("title");
-    $("#caption").fadeOut(3000, function () {
-      $("#caption").text(caption);
-      $("#caption").fadeIn(3000);
+      $("#caption").fadeOut(3000, function () {
+        $("#caption").text(caption);
+        $("#caption").fadeIn(3000);
 
-      //After the image fades in, add statements to change the font size of the caption to 2.0 em after three seconds using the animate method.
-      $("#caption").animate({ fontSize: "2em" }, 3000, function() {
+        //After the image fades in, add statements to change the font size of
+        //the caption to 2.0 em after three seconds using the animate method.
+        $("#caption").animate({ fontSize: "2em" }, 3000, function () {});
       });
     });
 
