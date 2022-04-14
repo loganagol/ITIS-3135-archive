57d39299c96f9af2da612aeca5ce74bc124bce57
loganagol <lafeid.314@gmail.com>
2022-04-14 18:36:54 -0400

    Complete Activity 14. Implemented iframes to display assignment so that assignment css and sitewide css can coexist.

--- itis3135/Feid-Activity14/Feid-Activity14.js
+++ itis3135/Feid-Activity14/Feid-Activity14.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+  $("#nav_list li").click(function (event) {
+    console.log("! list element clicked");
+
+    speakerfile = $(this).children("a").first().attr("title") + ".json";
+    console.log(speakerfile);
+
+    $.getJSON("json_files/" + speakerfile, function (data) {
+      $.each(data, function () {
+        $.each(this, function (key, value) {
+          $("main h1").html(value.title);
+          $("main h2").html(value.month);
+          $("main h3").html(value.speaker);
+          $("main img").attr("src", value.image);
+          $("main p").html(value.text);
+        });
+      });s
+    });
+  });
+
+  $.ajax({
+    type: "get",
+    url: "",
+  });
 }); // end ready
