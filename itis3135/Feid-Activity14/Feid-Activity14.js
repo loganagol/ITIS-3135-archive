4a0f9fae9cf04ecc019839f8a7a59d78ff19e862
loganagol <lafeid.314@gmail.com>
2022-04-16 17:38:07 -0400

    Corrections to activity 14, removed navbar iframe links.

--- itis3135/Feid-Activity14/Feid-Activity14.js
+++ itis3135/Feid-Activity14/Feid-Activity14.js
@@ -1,18 +1,34 @@
 $(document).ready(function () {
-  $("#nav_list li").click(function (event) {
+  $("#nav_list a").click(function (event) {
     console.log("! list element clicked");
 
-    speakerfile = $(this).children("a").first().attr("title") + ".json";
+    // speakerfile = $(this).children("a").first().attr("title") + ".json";
+    speakerfile = $(this).attr("title") + ".json";
+
     console.log(speakerfile);
 
     $.getJSON("json_files/" + speakerfile, function (data) {
       $.each(data, function () {
         $.each(this, function (key, value) {
-          $("main h1").html(value.title);
-          $("main h2").html(value.month);
-          $("main h3").html(value.speaker);
-          $("main img").attr("src", value.image);
-          $("main p").html(value.text);
+          // $("main h1").html(value.title);
+          // $("main h2").html(value.month);
+          // $("main h3").html(value.speaker);
+          // $("main img").attr("src", value.image);
+          // $("main p").html(value.text);
+
+          $("main h1").remove();
+          $("main h2").remove();
+          $("main h3").remove();
+          $("main img").remove();
+          $("main p").remove();
+
+          $("main").append(
+            "<h1>" + value.title + "</h2>" +
+            "<h2>" + value.month + "</h2>" +
+            "<h3>" + value.speaker + "</h3>" +
+            "<img src=" + value.image + ">" +
+            "<p>" + value.text + "</p>"
+          );
         });
       });
     });
