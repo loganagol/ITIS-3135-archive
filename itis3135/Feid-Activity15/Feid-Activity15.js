630a4770695300782fd1f60429dbd3a9a4fc2dcd
loganagol <lafeid.314@gmail.com>
2022-04-21 16:11:22 -0400

    Fulfilled activity 15 reqs.

--- itis3135/Feid-Activity15/Feid-Activity15.js
+++ itis3135/Feid-Activity15/Feid-Activity15.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    
+  $.ajax({
+    type: "get",
+    url: "facultyList.json",
+    dataType: "json",
+
+    success: function (data) {
+      $.each(data, function () {
+        $.each(this, function (key, value) {
+          let image = "<img src=" + value.image + ">";
+          let fullname = "<h2>" + value.full_name + "</h2>";
+          let department = "<h3>" + value.department + "</h3>";
+          let bio = "<p>" + value.bio + "</p>";
+
+          $("#faculty").append(image);
+          $("#faculty").append(fullname);
+          $("#faculty").append(department);
+          $("#faculty").append(bio);
+        });
+      });
+    },
+  });
 });
