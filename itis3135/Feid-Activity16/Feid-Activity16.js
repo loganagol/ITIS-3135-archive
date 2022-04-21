4ec375ce7226f4f83f596cfe810bc2f09d82005f
loganagol <lafeid.314@gmail.com>
2022-04-21 17:57:02 -0400

    Switched to https.

--- itis3135/Feid-Activity16/Feid-Activity16.js
+++ itis3135/Feid-Activity16/Feid-Activity16.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   let flickrUrl =
-    "http://api.flickr.com/services/feeds/photos_public.gne" +
+    "https://api.flickr.com/services/feeds/photos_public.gne" +
     "?id=82407828@N07" +
     "&format=json" +
     "&jsoncallback=?" +
